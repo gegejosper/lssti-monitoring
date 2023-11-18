00f99b899e35f2a3553ea87d042f985160c6263f
Gegejosper Ceniza <gegejosper@gmail.com>
2023-11-18 15:06:56 +0800

    change qrcode

--- resources/views/employees/employee.blade.php
+++ resources/views/employees/employee.blade.php
@@ -14,10 +14,25 @@
                     <div class="d-flex flex-center flex-column mb-5">
                         <!--begin::Avatar-->
                         <div class="mb-7 text-center">
-                        
-                        <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(200)->generate($employee->id)) !!} " class="img-thumbnail">
+                        <?php 
+                            //$url = config('app.url').'/customers/qrcode/'.$customer->account_number.'/'.$customer->id;
+                            $url ='#';
+                            function encodeURIComponent($str){
+                            $revert = array('%21'=>'!', '%2A'=>'*', '%27'=>"'", '%28'=>'(', '%29'=>')');
+                                return strtr(rawurlencode($str), $revert);
+                            }
+                            $qr_code = "https://chart.googleapis.com/chart?chs=200x200&cht=qr&chl=".$employee->id."&choe=UTF-8";
+                            echo '<a href="/customers/qrcode?url='.encodeURIComponent($qr_code).'" target="_blank">';
+                    
+                            // add the string in the Google Chart API URL
+                            $google_chart_api_url = "https://chart.googleapis.com/chart?chs=200x200&cht=qr&chl=".$url."&choe=UTF-8";
+
+                            // let's display the generated QR code
+                            echo "<img src='".$google_chart_api_url."' alt='".$employee->id."'></a>";
+                        ?>
+                        <!-- <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(200)->generate($employee->id)) !!} " class="img-thumbnail"> -->
                         <br>
-                        <a onclick="PrintImage('data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(200)->generate($employee->id)) !!} ')" class="btn btn-xs btn-icon btn-primary mt-5" print><i class="fa fa-print"> </i></a> 
+                        <a onclick="PrintImage('https://chart.googleapis.com/chart?chs=200x200&cht=qr&chl={{$employee->id}}&choe=UTF-8')" class="btn btn-xs btn-icon btn-primary mt-5" print><i class="fa fa-print"> </i></a> 
                         </div>
                         <!--end::Avatar-->
                         <!--begin::Name-->
@@ -26,6 +41,8 @@
                         <!--begin::Position-->
                         <div class="fs-5 fw-bold text-muted mb-6">{{$employee->status}}</div>
                         <!--end::Position-->
+
+                        
                         
                     </div>
                     <!--end::Summary-->
